ac4c6a7c37d2957e0439328f6e05aac49f17204c
Gabriel Amoirim <90463088+GabrielAmorim08@users.noreply.github.com>
2022-11-12 06:29:14 -0300

    Add files via upload

---
+++ back/script.sql
@@ -0,0 +1,37 @@
+use master
+go
+if exists(select * from sys.databases where name = 'tcc_site')
+drop database tcc_site
+go
+create database tcc_site
+go
+use tcc_site
+go
+
+create table Usuario(
+	ID int identity primary key,
+	nome varchar(100) not null,
+	email varchar(30) not null,
+	senha varchar(MAX) not null,
+	confsenha varchar(MAX) not null,
+	matricula varchar(10)
+);
+create table Token(
+	ID int identity primary key,
+	UserId int references Usuario(ID),
+	valor varchar(MAX) not null
+);
+
+create table PostAtividade(
+	ID int identity primary key,
+	UserID int references Usuario(ID),
+	Moment datetime not null,
+	Content varchar(MAX) not null
+
+);
+
+create table cargos(
+		ID int identity primary key,
+		CargoID int references Usuario(ID),
+
+);
